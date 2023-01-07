cf901f84d47a01d0c02efd7c781680a9358a6dc0
Hoyoon.Song <shuaru_43@naver.com>
2023-01-07 21:58:15 +0900

    초기 커밋.

---
+++ part03_01_linkedlist/part03_01_linkedlist.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream> 
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+class LinkedList 
+{
+private:
+	Node* head;
+	int size;
+public:
+	LinkedList() : head(NULL) {
+		head = new Node();
+		size = 0;
+	}
+
+	~LinkedList() {
+		while (!empty()) {
+			pop_front();
+		}
+	};
+
+	void push_front(int val) {
+		Node* new_node = new Node{ val, NULL };
+
+		new_node->next = head->next;
+		head->next = new_node;
+		size++;
+	};
+
+	void pop_front() {
+		if (head->next == NULL) {
+			std::cout << "size is 0 " << std::endl;
+			return;
+		}
+
+		Node* temp = head->next;
+		head->next = head->next->next;
+		std::cout << "Deleted value : " << temp->data << std::endl;
+		delete temp;
+	}
+
+	bool empty() const {
+		return head->next == NULL;
+	}
+
+	void print_all() const {
+		Node* cur = head->next;
+
+		while (cur != NULL) {
+			std::cout << cur->data << std::endl;
+			cur = cur->next;
+		}
+
+		std::cout << std::endl;
+	}
+};
+int main()
+{
+	LinkedList ll;
+	ll.push_front(10);
+	ll.push_front(20);
+	ll.push_front(30);
+	ll.print_all();
+
+	ll.pop_front();
+	ll.print_all();
+
+	ll.push_front(40);
+	ll.print_all();
+	
+	ll.~LinkedList();
+}
